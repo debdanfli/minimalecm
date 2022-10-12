0900abe2f4850b465e2adecba96ee751a142df66
Fligg, Daniel <daniel.fligg@lachmann-rink.de>
2022-10-12 10:00:46 +0200

    init

---
+++ app/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hey linux\n";
+
+  return 0;
+}
---
+++ src/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "test.hpp"
+
+void print_hello_world()
+{
+    printf("Hello World!\n");
+}
+
+void cout_hello_world()
+{
+    std::cout << "Hello World!" << std::endl;
+}
